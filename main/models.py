c2d55b39d8ddae94f5138dc08e806a3297688c8a
yessine <yacinbnsalh@gmail.com>
2021-06-24 20:48:23 +0100

    second version

--- main/models.py
+++ main/models.py
@@ -11,12 +11,15 @@ class Category(models.Model):
 
 
 class Formation(models.Model):
+    ETAT=(('activé','activé'),
+            ('non activé','non activé'))
+
     name = models.CharField(max_length=50 , null = True)
     duree = models.IntegerField(null = True )
     #logo = models.ImageField(blank=True , null = True , upload_to ="logos" ,default='logo/default.jpg')
     logo = models.ImageField(upload_to='logos', blank=True, null=True , default='logo/default.jpg')
     category = models.ForeignKey(Category , on_delete=models.CASCADE)
-    etat = models.BooleanField(default=False)
+    etat = models.CharField(max_length=200, null=True, choices=ETAT)
     description = models.CharField(max_length=500 , null =True) 
     date_created = models.DateTimeField(auto_now_add=True , null = True)
     def __str__(self):
